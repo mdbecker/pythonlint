6c7395e834974e908107de369248566ae8f4d4f4
Michael Becker <mike@beckerfuffle.com>
2013-11-27 15:17:52 -0500

    generate: wheel -> pep8

--- generate.py
+++ generate.py
@@ -1,6 +1,6 @@
-from svg_wheel import generate_svg_wheel
+from svg_lint import generate_svg_wheel
 from utils import (
-    annotate_wheels,
+    annotate_pep8,
     get_top_packages,
     remove_irrelevant_packages,
     save_to_file,
@@ -12,7 +12,7 @@ TO_CHART = 360
 
 def main():
     packages = remove_irrelevant_packages(get_top_packages(), TO_CHART)
-    annotate_wheels(packages)
+    annotate_pep8(packages)
     save_to_file(packages, 'results.json')
     generate_svg_wheel(packages, TO_CHART)
 
